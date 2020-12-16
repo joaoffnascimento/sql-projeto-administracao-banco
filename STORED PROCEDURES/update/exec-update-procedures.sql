2951b162014e5ecd1ef3cbaf1dc75f0bef5956ef
joaoffnascimento <thecurrentuser@live.com>
2020-12-15 23:39:56 -0300

    feat: added exec updated procedures

---
+++ STORED PROCEDURES/update/exec-update-procedures.sql
@@ -0,0 +1,51 @@
+-- Created by: João Felipe Fonseca Nascimento
+-- joao.nascimento98@academico.ifs.edu.br - 2020
+
+USE Zobooaereo
+
+GO
+
+EXECUTE dbo.USP_MODIFICAR_PAIS
+    1,
+    "ALEMANHA"
+GO
+
+EXECUTE dbo.USP_MODIFICAR_CIDADE
+    1,
+    "LAGARTO"
+GO
+
+EXECUTE dbo.USP_MODIFICAR_COMPANHIA_AEREA
+    1,
+    "A expressão Lorem ipsum em design gráfico."
+    
+GO
+
+EXECUTE dbo.USP_MODIFICAR_TIPO_AVIAO
+    1,
+    150
+GO
+
+EXECUTE dbo.USP_MODIFICAR_VOO
+    2,
+    16,
+    1931
+GO
+
+EXECUTE dbo.USP_MODIFICAR_AEROPORTO
+    1,
+    "simply dummy text of the printing and typesetting industry. Lorem Ipsum"
+GO
+
+EXECUTE dbo.USP_MODIFICAR_ESCALA
+    1,
+    3,
+    5
+GO
+
+EXECUTE dbo.USP_MODIFICAR_TARIFA
+    1,
+    1,
+    2,
+    55
+GO
